fd160f5cfa693560930dfa1607011b54690f3404
Matt George <mbgeorge48@gmail.com>
2026-04-14 20:58:09 +0100

    Removing comments

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # This automatically looks for SUPERHERO_API_TOKEN in your .env
     superhero_api_token: str = ""
     superhero_api_base_url: str = ""
 
-    # Tells Pydantic where to find the file
     model_config = SettingsConfigDict(env_file=".env")
 
 
-# Create a single instance to be used everywhere
 settings = Settings()
